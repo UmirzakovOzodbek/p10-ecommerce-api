e74d5b2946896c741ba637b3cbf1763f94f59ea1
user <ozodbekumurzakov2006@gmail.com>
2023-05-11 21:19:00 +0500

    category, products api added .gitignore

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import RegisterView, CustomTokenObtainPairView, ProfileView
+from users.views import RegisterView, CustomTokenObtainPairView, ProfileView, SendEmailVerificationCodeView, \
+    CheckEmailVerificationCodeView, CheckEmailVerificationCodeWithParams
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("email/verification/", SendEmailVerificationCodeView.as_view(), name="send-email-code"),
+    path("email/check-verification/", CheckEmailVerificationCodeView.as_view(), name="check-email-code"),
+    path("email/check-verification-code/", CheckEmailVerificationCodeWithParams.as_view(), name="check-email"),
 ]
